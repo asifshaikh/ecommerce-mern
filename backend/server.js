c757d1b5ac08dc2c79d2d4138ab824bc6330c09f
asifshaikh <shaikha015@gmail.com>
2024-12-16 02:01:29 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
+import couponRoutes from './routes/coupon.route.js';
 import connectDB from './libs/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/product', productRoutes);
 app.use('/api/cart', cartRoutes);
+app.use('/api/coupon', couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
