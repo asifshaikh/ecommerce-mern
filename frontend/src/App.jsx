6f193160612ba081594ec3d477d9ea96e59e83b9
asifshaikh <shaikha015@gmail.com>
2024-12-23 16:58:39 +0530

    Update routing in App.jsx to redirect logged-in users from LoginPage to HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,10 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import { Toaster } from 'react-hot-toast';
+import { useUserStore } from './stores/useUserStore';
 
 function App() {
+  const { user } = useUserStore();
   return (
     <div className='min-h-screen bg-gray-900 text-white relative overflow-hidden'>
       {/* Background gradient */}
@@ -20,7 +22,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/login' element={<LoginPage />} />
+          <Route path='/login' element={user ? <HomePage /> : <LoginPage />} />
         </Routes>
       </div>
       <Toaster />
