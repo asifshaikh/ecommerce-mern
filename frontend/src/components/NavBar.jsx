2e3bd45999a1d939235683d04e40be2afceb7706
asifshaikh <shaikha015@gmail.com>
2024-12-23 22:34:21 +0530

    Rename E-Commerce to MernMart and add CategoryItem component to HomePage for category display

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
             to='/'
             className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'
           >
-            E-Commerce
+            MernMart
           </Link>
 
           <nav className='flex flex-wrap items-center gap-4'>
