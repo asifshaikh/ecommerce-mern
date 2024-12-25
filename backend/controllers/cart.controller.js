ca25bff4208dfab063994eb12e6042e09beda63b
asifshaikh <shaikha015@gmail.com>
2024-12-25 17:36:06 +0530

    Enhance getCartProducts controller to include product quantity and improve error logging

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -64,13 +64,18 @@ export const updateQuantity = async (req, res) => {
 export const getCartProducts = async (req, res) => {
   try {
     const products = await Product.find({ _id: { $in: req.user.cartItems } });
+
+    // add quantity for each product
     const cartItems = products.map((product) => {
-      const item = req.user.cartItems.find((item) => item.id === product.id);
-      return { ...product, quantity: item.quantity };
+      const item = req.user.cartItems.find(
+        (cartItem) => cartItem.id === product.id
+      );
+      return { ...product.toJSON(), quantity: item.quantity };
     });
+
     res.json(cartItems);
   } catch (error) {
-    console.log('Error in getCartProducts: ', error.message);
-    res.status(500).json({ error: error.message, message: 'Server Error' });
+    console.log('Error in getCartProducts controller', error.message);
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
