0266f7fe5cde9203cbcdfd4d530fc9134753c69f
asifshaikh <shaikha015@gmail.com>
2024-12-25 22:16:17 +0530

    Add analytics data retrieval and visualization; implement analytics dashboard with user, product, sales, and revenue metrics

--- backend/controllers/analytic.controller.js
+++ backend/controllers/analytic.controller.js
@@ -1,5 +1,7 @@
-import User from '../models/user.model.js';
+import Order from '../models/order.model.js';
 import Product from '../models/product.model.js';
+import User from '../models/user.model.js';
+
 export const getAnalyticsData = async () => {
   const totalUsers = await User.countDocuments();
   const totalProducts = await Product.countDocuments();
@@ -7,15 +9,26 @@ export const getAnalyticsData = async () => {
   const salesData = await Order.aggregate([
     {
       $group: {
-        _id: null,
+        _id: null, // it groups all documents together,
         totalSales: { $sum: 1 },
         totalRevenue: { $sum: '$totalAmount' },
       },
     },
   ]);
-  const { totalSales, totalRevenue } = salesData[0] || {};
-  return { totalUsers, totalProducts, totalSales, totalRevenue };
+
+  const { totalSales, totalRevenue } = salesData[0] || {
+    totalSales: 0,
+    totalRevenue: 0,
+  };
+
+  return {
+    users: totalUsers,
+    products: totalProducts,
+    totalSales,
+    totalRevenue,
+  };
 };
+
 export const getDailySalesData = async (startDate, endDate) => {
   try {
     const dailySalesData = await Order.aggregate([
@@ -62,6 +75,7 @@ export const getDailySalesData = async (startDate, endDate) => {
     throw error;
   }
 };
+
 function getDatesInRange(startDate, endDate) {
   const dates = [];
   let currentDate = new Date(startDate);
--- backend/routes/analytic.route.js
+++ backend/routes/analytic.route.js
@@ -9,16 +9,20 @@ const router = express.Router();
 
 router.get('/', protectedRoute, adminRoute, async (req, res) => {
   try {
-    const analyticData = await getAnalyticsData();
+    const analyticsData = await getAnalyticsData();
 
     const endDate = new Date();
-    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000); // 7 days
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
     const dailySalesData = await getDailySalesData(startDate, endDate);
 
-    res.json({ analyticData, dailySalesData });
+    res.json({
+      analyticsData,
+      dailySalesData,
+    });
   } catch (error) {
-    console.log('Error in getAnalyticsData: ', error.message);
-    res.status(500).json({ error: error.message, message: 'Server Error' });
+    console.log('Error in analytics route', error.message);
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 });
 
